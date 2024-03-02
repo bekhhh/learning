8c88273a0b206c063edd1bfb7cfb276b6602effa
bekhhh <narutobexruz@gmail.com>
2024-03-02 14:30:50 +0300

    learning

---
+++ 3/Program.cs
@@ -0,0 +1,25 @@
+﻿var color = ConsoleColor.Yellow;
+while (true)
+{
+    var input = Console.ReadLine();
+    if (input == "switch")
+    {
+        if (color == ConsoleColor.Yellow)
+        {
+            color = ConsoleColor.Red;
+            Console.ResetColor();
+        }
+        else
+        {
+
+            color = ConsoleColor.Yellow;
+        }
+        Console.WriteLine("Цвет изменен");
+    }
+    else
+    {
+        Console.ForegroundColor = color;
+        Console.WriteLine(input);
+        Console.ForegroundColor = ConsoleColor.White;
+    }
+}
---
+++ 6/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Введите текст:");
+while (true) 
+{ 
+var input = Console.ReadLine();
+    var words = input.Split(' ');
+    var dictionary = new Dictionary<string, List<int>>();
+    for (int i = 0; i < words.Length; i++) 
+    { 
+    var  word = words[i];
+        if (!dictionary.ContainsKey(word))
+        {
+            dictionary[word] = new List<int>();
+        }
+        else 
+        { 
+        dictionary.Add(word, new List<int>());
+        }
+       
+    }
+}
