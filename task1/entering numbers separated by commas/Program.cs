484b16ae4b7beea440dba6e7f461a06decab7591
bekhhh <narutobexruz@gmail.com>
2024-03-12 17:11:16 +0300

    обновил старый гитигноре

---
+++ task1/entering numbers separated by commas/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Channels;
+Console.WriteLine("Введите числа через запятую");
+while (true) 
+{ 
+
+var input = Console.ReadLine();
+    if (!Regex.IsMatch(input, @"^\d+(,\d+)*$")) //задаю шаблон 
+    {
+        Console.WriteLine("Ошибка: ввод не соответствует формату.");
+        continue;
+    }
+    else 
+    {
+        var numbers = input.Split(',').Select(x => Convert.ToInt32(x)).ToArray();// тут разбил (заменил ConvertAll) сконвертировал в массив(toArray)
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            for (int j = 0; j < numbers.Length - i - 1; j++) //сортировка пузырьком
+            {
+                if (numbers[j] > numbers[j + 1])
+                {
+                    int temp = numbers[j];
+                    numbers[j] = numbers[j + 1];
+                    numbers[j + 1] = temp;
+                }
+            }
+        }
+        Console.WriteLine("Отсортированные числа:");
+        Console.WriteLine(string.Join(", ", numbers)); //собрал И ВЫВЕЛ НА КОНСОЛЬ
+
+    }
+
+}
