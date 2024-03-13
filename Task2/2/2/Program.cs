b043d40a2ebc5023f8d8ef566e760f58ea7cf2cb
bekhhh <narutobexruz@gmail.com>
2024-03-13 10:56:48 +0300

    task 2

---
+++ Task2/2/2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System.Diagnostics.Tracing;
+using static System.Net.Mime.MediaTypeNames;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        while (true)
+        {
+            var input = Console.ReadLine();
+            if (input == "End")
+            {
+                break;
+            }
+            else
+            {
+                var words = input.Split(' '); //разделил
+                for (int i = 0; i < words.Length; i++)
+                {
+                    var reversedWord = "";
+                    for (int j = words[i].Length - 1; j >= 0; j--)
+                    {
+                        reversedWord += words[i][j]; // тут начиная с первого for идет процесс переворачивания
+                    }
+                    words[i] = reversedWord; // тут заменил переменной
+                }
+                var reversedString = string.Join(" ", words); //собрал
+                Console.WriteLine(reversedString); // вывел на консоль
+            }
+        }
+    }
+}
