0e59cfb5633b91ab3c6095ca27b335c97278b7a8
bekhhh <narutobexruz@gmail.com>
2024-03-15 16:24:26 +0300

    загружаю таск 6

---
+++ Task6/6/Program.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.Tracing;
+
+Console.WriteLine("Введите текст: ");
+while (true)
+{
+    var input = Console.ReadLine();
+    var words = input.Split(' ');
+    var dictionary = new Dictionary<string, List<int>>();
+    for (int i = 0; i < words.Length; i++)
+    {
+        var word = words[i];
+        if (!dictionary.ContainsKey(word))
+        {
+            dictionary[word] = new List<int>();
+        }
+        dictionary[word].Add(i + 1);
+    }
+    foreach (var item in dictionary)
+    {
+        Console.WriteLine($"Слово:\"{item.Key}\", количество: {item.Value.Count}, позиция: {string.Join(" ", item.Value)} ");
+    }
+}
