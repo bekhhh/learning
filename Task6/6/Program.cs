56e8553023e5838a9f9919e608919ec6ea088312
bekhhh <narutobexruz@gmail.com>
2024-03-28 19:00:40 +0300

    добавил проверку на пустую строку после разбиения ввода на слова,использовал паттерн типов и во время разделения ввода по пробелам удалил все пробелы

--- Task6/6/Program.cs
+++ Task6/6/Program.cs
@@ -4,7 +4,12 @@ Console.WriteLine("Введите текст: ");
 while (true)
 {
     var input = Console.ReadLine();
-    var words = input.Split(' ');
+    if (string.IsNullOrWhiteSpace(input))
+    {
+        Console.Write("Ошибка: Введите текст ");
+        continue;
+    }
+    var words = input.Split(' ').Select(x => x.Trim()).ToArray();
     var dictionary = new Dictionary<string, List<int>>();
     for (int i = 0; i < words.Length; i++)
     {
@@ -15,8 +20,8 @@ while (true)
         }
         dictionary[word].Add(i + 1);
     }
-    foreach (var item in dictionary)
+    foreach (var (item, positions) in dictionary)
     {
-        Console.WriteLine($"Слово:\"{item.Key}\", количество: {item.Value.Count}, позиция: {string.Join(" ", item.Value)} ");
+        Console.WriteLine($"Слово:\"{item}\", количество: {positions.Count}, позиция: {string.Join(" ", positions)} ");
     }
 }
