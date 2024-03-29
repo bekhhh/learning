f6fe6ea5fafc3a9a2214b9a62ac78bc612818af9
bekhhh <narutobexruz@gmail.com>
2024-03-29 15:38:40 +0300

    убрал else

--- Task2/2/Program.cs
+++ Task2/2/Program.cs
@@ -1,7 +1,4 @@
-﻿using System.Diagnostics.Tracing;
-using static System.Net.Mime.MediaTypeNames;
-
-public class Program
+﻿public class Program
 {
     private static void Main(string[] args)
     {
@@ -12,21 +9,18 @@ public class Program
             {
                 break;
             }
-            else
+            var words = input.Split(' '); //разделил
+            for (int i = 0; i < words.Length; i++)
             {
-                var words = input.Split(' '); //разделил
-                for (int i = 0; i < words.Length; i++)
+                var reversedWord = "";
+                for (int j = words[i].Length - 1; j >= 0; j--)
                 {
-                    var reversedWord = "";
-                    for (int j = words[i].Length - 1; j >= 0; j--)
-                    {
-                        reversedWord += words[i][j]; // тут начиная с первого for идет процесс переворачивания
-                    }
-                    words[i] = reversedWord; // тут заменил переменной
+                    reversedWord += words[i][j]; // тут начиная с первого for идет процесс переворачивания
                 }
-                var reversedString = string.Join(" ", words); //собрал
-                Console.WriteLine(reversedString); // вывел на консоль
+                words[i] = reversedWord; // тут заменил переменной
             }
+            var reversedString = string.Join(" ", words); //собрал
+            Console.WriteLine(reversedString); // вывел на консоль           
         }
     }
 }
