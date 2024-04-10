debf7d3dac9f2bc40e4b576795561325be1880f4
bekhhh <narutobexruz@gmail.com>
2024-04-10 10:18:13 +0300

    вренменно

---
+++ Task4/4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.IO;
+using System.Linq;
+using System.Text.RegularExpressions;
+
+var dictionary = new Dictionary<string,int>();
+var filePath =@"C:\Projects\forTask.txt";
+if (File.Exists(filePath))
+{
+    string[] lines = File.ReadAllLines(filePath);
+    foreach (string line in lines)
+    {
+        var arrayOfStrings = line.Split(',')
+            .Select(x => x.Trim())
+            .Where(x => !string.IsNullOrEmpty(x))
+            .ToArray();
+        foreach (var symbols in arrayOfStrings) 
+        {
+            foreach (var text in File.ReadLines(filePath).Skip(1))
+            {
+                if (dictionary.ContainsKey(symbols)) 
+                {
+                    dictionary[symbols]++;
+                }
+            }
+        }       
+    }    
+}
