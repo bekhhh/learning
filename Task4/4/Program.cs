4c3dad71f1f22b99875e4fa9232db4c1bf144f86
bekhhh <narutobexruz@gmail.com>
2024-04-23 14:41:19 +0300

    готово)

--- Task4/4/Program.cs
+++ Task4/4/Program.cs
@@ -1,46 +1,34 @@
-﻿using System.Diagnostics.Metrics;
+﻿using System;
 using System.IO;
 using System.Linq;
-using System.Text.RegularExpressions;
 
-var dictionary = new Dictionary<string, List<int>>();
 Console.WriteLine("Введите путь к файлу ");
 var filePath = Console.ReadLine(); //C:\Projects\forTask.txt => мой путь к файлу
-if (!File.Exists(filePath)) // проверяем на наличие файла
+if (!File.Exists(filePath))
 {
     Console.WriteLine("Файл не существует");
     return;
 }
-var lines = File.ReadAllLines(filePath); // читаем файл
+var lines = File.ReadAllLines(filePath);
 var firstLine = lines[0];
 var arrayOfSymbols = firstLine.Split(',')
                        .Select(x => x.Trim())
                        .Where(x => !string.IsNullOrEmpty(x))
-                       .ToArray(); // тут разделили и убрали лишние пробелы
-for (int i = 1; i < lines.Length; i++) // начинаем с первого индекса, тк как 0 индекс нам не нужен(там наши символы)
-{
-    var currentLine = lines[i];
-    foreach (var symbol in arrayOfSymbols)
-    {
-        int count = currentLine.Count(c => c.ToString() == symbol); // подсчет количества символов в оставшиеся 2 строках 
-        if (!dictionary.ContainsKey(symbol))
-        {
-            dictionary[symbol] = new List<int>(); // если не встречался, то добавляем 
-        }
-        dictionary[symbol].Add(count); // тут добавляем количество вхождений в строку, если встртечался
-    }
-}
-foreach (var с in dictionary)
-{
-    Console.WriteLine($"{с.Key}: {string.Join(", ", с.Value)}"); // в таске не обязательно, но я вывел на всякий
-}
+                       .Select(x => x[0])
+                       .ToArray();
 var newFilePath = @"C:\Projects";
-var newFile = "forTask " + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt"; // надюсь правильно понял про суфикс
+var newFile = "forTask " + DateTime.Now.ToString("yyyy-MM-dd_HH-mm-ss") + ".txt";
 string fullFilePath = Path.Combine(newFilePath, newFile);
-using (StreamWriter writer = new StreamWriter(fullFilePath)) // тут уже идет запись в новый файл
+using (StreamWriter writer = new StreamWriter(fullFilePath)) 
 {
-    foreach (var v in dictionary)
+    for (int i = 1; i < lines.Length; i++)
     {
-        writer.WriteLine($"{v.Key}: {string.Join(", ", v.Value)}");
+        var currentLine = lines[i];
+        foreach (char symbol in arrayOfSymbols)
+        {
+            int count = currentLine.Count(c => c == symbol);
+            writer.Write($"{count} ");
+        }
+        writer.WriteLine();
     }
 }
