ce2096616960b79f5947921ac7eb31895a396689
bekhhh <narutobexruz@gmail.com>
2024-05-07 20:14:11 +0300

    1

---
+++ Task7.1/Task7.1/DynamicArray.cs
@@ -0,0 +1,30 @@
+﻿namespace Task7._1
+{
+    public class DynamicArray<T>
+    {
+
+        T[] inputArray;
+         void InputHandler <T>() 
+        {
+            while (true) 
+            { 
+            var input = T.Parse( Console.ReadLine());
+            }
+        }
+        public T Lenght { get; set; }
+        void DeletionByIndex() 
+        { 
+        
+        }
+
+        void AddToEnd() 
+        { 
+        
+        }
+
+        void GetByIndex() 
+        { 
+        
+        }
+    }
+}
