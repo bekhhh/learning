5fc192f2f2d1186651d4827650aa4474cb3cd268
bekhhh <narutobexruz@gmail.com>
2024-06-07 13:51:13 +0300

    qwe

---
+++ Task7.1/Task7.1/InputController.cs
@@ -0,0 +1,23 @@
+﻿namespace Task7._1
+{
+    public class InputController
+    {
+        public void HandlerInput() 
+        {
+            while (true)
+            {
+                var input = Console.ReadLine();
+                if (string.IsNullOrEmpty(input))
+                {
+                    Console.WriteLine("Введена пустая строка");
+                    continue;
+                }
+                var words = input.Split(' ')
+                .Select(x => x.Trim())
+                .Where(x => !string.IsNullOrEmpty(x))
+                .ToArray();                
+            }
+        }
+    }
+}
+
