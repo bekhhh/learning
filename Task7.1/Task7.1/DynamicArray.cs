a9c5e77d07fe2266c662978e87a62acdd18a696e
bekhhh <narutobexruz@gmail.com>
2024-06-07 11:43:13 +0300

    gh

--- Task7.1/Task7.1/DynamicArray.cs
+++ Task7.1/Task7.1/DynamicArray.cs
@@ -8,47 +8,50 @@ namespace Task7._1
         private T[] _array;
         private int _size;
         private int _currentIndex;
-        public int Lenght { get; set; }
+        public int Length { get; set; }
+
         public DynamicArray()
         {
+            _size = 1;
             _array = new T[_size];
-            Lenght = _array.Length;
+            Length = 0;
         }
 
         public void AddToEnd(T value)
         {
-            if (_array == null)
-            {
-                _array = new T[1];
-                _currentIndex = 0;
-            }
-            else if (_currentIndex == _array.Length)
+            if (_currentIndex == _array.Length)
             {
                 T[] newArray = new T[_array.Length * 2];
                 Array.Copy(_array, newArray, _array.Length);
                 _array = newArray;
+                _size = _array.Length;
             }
             _array[_currentIndex] = value;
             _currentIndex++;
+            Length++;
         }
 
         public void RemoveElement(int index)
         {
-            if (index < 0 || index >= _array.Length)
+            if (index < 0 || index >= _currentIndex)
             {
                 throw new IndexOutOfRangeException("Индекс выходит за пределы диапазона");
             }
-
-            _array[index] = default;
+            for (int i = index; i < _currentIndex - 1; i++)
+            {
+                _array[i] = _array[i + 1];
+            }
+            _array[_currentIndex - 1] = default;
+            _currentIndex--;
+            Length--;
         }
 
         public T GetByIndex(int index)
         {
-            if (index < 0 || index >= _size)
+            if (index < 0 || index >= _currentIndex)
             {
                 throw new IndexOutOfRangeException("Индекс выходит за пределы диапазона");
             }
-
             return _array[index];
         }
     }
