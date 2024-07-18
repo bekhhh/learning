7bccb9106d6d417b6313a339e200dec540b70775
bekhhh <narutobexruz@gmail.com>
2024-07-18 10:32:11 +0300

    qwe

---
+++ Task9/UserInputTest/ParsingResultTest.cs
@@ -0,0 +1,174 @@
+namespace UserInputTest
+{
+    public class ParsingResultTest
+    {
+        [Fact]
+        public void Test_First_Word() 
+        {
+            //Arrange
+            var result = new CommandParser();
+
+            //Act
+
+
+            //Assert
+
+        }
+
+        [Fact]
+        public void Test_Lenght_Of_Input_When_First_Word_Get()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Second_Word_When_First_Word_Get()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Third_Word_When_First_Word_Get_And_Second_Word_Items_Or_Description()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Lenght_Of_Input_When_First_Word_Show()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Second_Word_When_First_Word_Show()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Lenght_Of_Input_When_First_Word_Start()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Second_Word_When_First_Word_Start()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Start_Word_Reentry_Test()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Lenght_Of_Input_When_First_Word_Add()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Second_Word_When_First_Word_Add()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Third_And_Fourth_Word_When_First_Word_Add_Second_Word_Ability()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Fiveth_Word_When_First_Word_Add_And_Second_Word_Ability()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+
+        [Fact]
+        public void Test_Sixth_Word_When_First_Word_Add_And_Second_Word_Ability()
+        {
+            //Arrange
+
+
+            //Act
+
+
+            //Assert
+        }
+    }
+}
